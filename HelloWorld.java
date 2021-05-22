a4de50d29de8cbad56c8679904b479f14be7649f
yuanzw <yuanzw@shanghaitech.edu.cn>
2021-05-22 13:27:19 +0800

    init all

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    //java程序的入口方法，程序从这里开始执行；
+    public static void main(String[] args)
+    {
+        //向控制台打印一条语句；
+        System.out.println("Hello World!");
+    }
+}
